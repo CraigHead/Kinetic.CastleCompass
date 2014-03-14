7265a334819d7d205e6e701b8f54b2332cabbf22
CraigHead <youngcraig@gmail.com>
2014-03-14 12:04:52 -0600

    Removing stupid dlls. Refactored data classes to reuse common functionality.

--- Kinetic.CastleCompass.Core/InNOut.cs
+++ Kinetic.CastleCompass.Core/InNOut.cs
@@ -9,31 +9,11 @@ namespace Kinetic.CastleCompass.Core
 {
     public class InNOut : CoordinateResource
     {
-        private const string RESOURCENAME = @"Kinetic.CastleCompass.Core.Data.In N Out.csv";
-        public override bool TryOpenLongLats()
+        internal override string ResourceName
         {
-            using (Stream resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(RESOURCENAME))
+            get
             {
-                if (resourceStream == null)
-                    return false;
-
-                using (TextReader reader = new StreamReader(resourceStream))
-                {
-                    string line;
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        double latitude;
-                        double longitude;
-
-                        string[] pairs = line.Split(',');
-
-                        if (double.TryParse(pairs[0], out latitude) && double.TryParse(pairs[1], out longitude))
-                        {
-                            Coordinates.Add(new Coords { Longitude = longitude, Latitude = latitude });
-                        }
-                    }
-                }
-                return true;
+                return @"Kinetic.CastleCompass.Core.Data.In N Out.csv";
             }
         }
     }
